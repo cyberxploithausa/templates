d556a3f8862337ecdc967f25caa9586605b0191e
cyberxploithausa <cyberxploit166@gmail.com>
2025-08-10 22:29:32 +0100

    feat: mynotes

---
+++ my_notes.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Notification
+export DISPLAY=:0
+export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$(id -u)/bus"
+notify-send "About To Backup" "Your notes are about to synced to Github at $(date)"
+cd /home/cyberxploit/Desktop/projects/ctfs/Notes/CTF-Notes || exit
+git add .
+git commit -m "Auto-backup: $(date '+%Y-%m-%d %H:%M:%S')"
+git push
+git push https://cyberxploithausa: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@github.com/cyberxploithausa/ctf-notes.git
+
+# Notification
+export DISPLAY=:0
+export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$(id -u)/bus"
+notify-send "Obsidian Backup" "Notes synced to GitHub at $(date)"
+
